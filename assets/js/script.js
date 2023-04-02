1904a2894ff0805fa4a16c3d4e0cd65d374f7cfd
PersianPatriotVPN <129263458+PersianPatriotVPN@users.noreply.github.com>
2023-04-02 22:22:44 +0330

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+const sectionMain = `
+<div>
+<center>
+<img
+  src="./assets/img/logo3.png"
+  alt="Logo"
+  class="mt-4"
+  style="width: 210px; border: unset !important;"
+/>
+<h3 class="mt-1">
+  <a
+  style="text-decoration: unset;"
+  href="./index.html"
+  target="_self"
+  >
+    صفحه اصلی
+  <br />
+
+  
+  <code 
+  style="font-size:14px;"
+  >
+  https://PersianPatriotVPN.github.io
+  </code>
+  
+
+  </a>
+</h3>
+<p
+class="lead mt-4"
+style="font-size: 21px;"
+
+>
+  این سیستم جهت رفع تحریم‌های ظالمانه 
+  
+  <img src="./assets/img/melting-face_1fae0.png"
+  style="width: 28px; border: unset !important;"
+  />
+  جهت اتصال به اینترنت می‌باشد
+</p>
+</center>
+</div>
+`;
+document.getElementById("sectionMain").innerHTML = sectionMain;
+
+const helpConnection = (input = "", id) => {
+  return `
+  
+  `;
+};
